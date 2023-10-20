0b6436a47a06901976b0c8476ac2712c5a3455d4
Shujat-Ali-kay <148149834+Shujat-Ali-kay@users.noreply.github.com>
2023-10-20 19:44:57 +0500

    Add files via upload

---
+++ appoi1.php
@@ -0,0 +1,22 @@
+<?php
+ $connec = mysqli_connect("localhost","root","","doc");
+ ?>
+                                
+ <?php
+$doc=$_POST['doc'];
+$patient=$_POST['patient'];
+$phone=$_POST['phone'];
+$date=$_POST['date'];
+$time=$_POST['time'];
+                                
+$q = "INSERT INTO login(s_doc,s_patient,s_phone,s_date,s_time) VALUES('$doc','$patient','$phone','$date','$time')";
+$RESULT = mysqli_query($connec,$q);
+if(!$RESULT){
+echo "error connection";
+}
+else
+{
+header('location:booked.html');  
+
+}
+?>
